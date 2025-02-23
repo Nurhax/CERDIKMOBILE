112a2106670c670ec6c8379bf81dc7b760cff74f
Iqbal-Nurhaq <iqbalnur2009@gmail.com>
2025-02-23 19:30:26 +0700

    Delete lib/Models directory
    unused directory

--- lib/Models/Jadwal.dart
+++
@@ -1,8 +0,0 @@
-import 'obat.dart';
-class Jadwal {
-  int? IDJadwal, IDPasien, IDObat;
-  String? Gejala,Dosis,Start_Date,End_Date;
-  bool? IsConfirmedNakes;
-
-  List<Map<Obat, dynamic>>? obatPasien;
-}
