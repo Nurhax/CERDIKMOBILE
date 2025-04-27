9a60c918b741a6630747d6258a803a3ef288693b
Iqbal-Nurhaq <iqbalnur2009@gmail.com>
2025-04-27 15:12:45 +0700

    Versi 0.0.7

--- backendFolder/APIPPB/insert_record_obat.php
+++ backendFolder/APIPPB/insert_record_obat.php
@@ -3,36 +3,49 @@
 include("db.php");
 $con = dbconnection();
 
+// Check for missing POST parameters
 if(isset($_POST["nama"])){
     $nama = $_POST["nama"];
 }
 else return;
-
 
 if(isset($_POST["jenis"])){
     $jenis = $_POST["jenis"];
 }
 else return;
 
-
-if(isset($_POST["saranPenyajian"])){
-    $saranPenyajian = $_POST["saranPenyajian"];
+if(isset($_POST["dosis"])){
+    $dosisobat = $_POST["dosis"];  // map 'dosis' to 'dosisobat'
 }
 else return;
 
-$query = "INSERT INTO `obat`(`nama`, `jenis`, `saranPenyajian`) VALUES 
-('$nama','$jenis','$saranPenyajian')";
+if(isset($_POST["gejala"])){
+    $gejalaobat = $_POST["gejala"];  // map 'gejala' to 'GejalaObat'
+}
+else return;
 
- $exe=mysqli_query($con,$query);
+if(isset($_POST["deskripsi"])){
+    $deskripsi = $_POST["deskripsi"];
+}
+else return;
 
- $arr=[];
- 
+if(isset($_POST["ukuran"])){
+    $ukuran = $_POST["ukuran"];  // map 'satuan' to 'ukuran'
+}
+else return;
+
+$query = "INSERT INTO `obat`(`nama`, `jenis`, `dosisobat`, `GejalaObat`, `ukuran`, `deskripsi`) 
+          VALUES ('$nama','$jenis','$dosisobat','$gejalaobat','$ukuran','$deskripsi')";
+
+$exe = mysqli_query($con, $query);
+$arr = [];
+
 if($exe){
-    $arr["success"]="true";
-}else{
-    $arr["success"]="false";
+    $arr["success"] = "true";
+} else {
+    $arr["success"] = "false";
 }
 
-
 print(json_encode($arr));
+
 ?>
